379efd2da22b1fadcc7cde0d59e8ea45a5cf4a6b
James Prior <jim.prior@accenture.com>
2020-01-07 15:57:35 -0500

    parse_url.py: Create.

---
+++ parse_url.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+from urllib.parse import urlparse, parse_qs
+
+
+def format_parsed_url(parsed_url):
+    # q = urlparse(parsed_url)
+
+    field_names = ('scheme', 'netloc', 'path', 'params', 'query', 'fragment')
+    output = []
+    for key, value in zip(field_names, parsed_url):
+        output.append(f'{key}:\t{value!r}\n')
+    return ''.join(output)
+
+
+def format_query(query):
+    output = []
+    for key, value in parse_qs(query).items():
+        output.append(f'{key}:\t{value!r}\n')
+    return ''.join(output)
+    
+
+def main(lines):
+    for line in lines:
+        url = line.strip()
+        print(f'url:\t{url!r}')
+        parsed_url = urlparse(url)
+        print(format_parsed_url(parsed_url), end='')
+        query = parsed_url.query
+        print(format_query(query), end='')
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.stdin)
---
+++ tests/test_parse_urls.py
@@ -0,0 +1,70 @@
+from urllib.parse import urlparse, parse_qs
+
+import pytest
+
+from parse_url import format_parsed_url, format_query, main
+
+'''
+url = 'laksjdf' 
+parse_url
+return string and parse_object
+'''
+
+def test_format_parsed_url():
+    url = (
+        'https://foo.com/bar/digest'
+        '?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'
+    )
+    parsed_url = urlparse(url)
+    expected = (
+        "scheme:\t'https'\n"
+        "netloc:\t'foo.com'\n"
+        "path:\t'/bar/digest'\n"
+        "params:\t''\n"
+        "query:\t'lat=here&link=https%3A%2F%2Fbaz.com%2F"
+            "%23%2Fhear%2F%3Fr%3Djoe'\n"
+        "fragment:\t''\n"
+    )
+    actual = format_parsed_url(parsed_url)
+    assert expected == actual
+
+
+def test_format_query():
+    query = 'lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'
+
+    expected = (
+        "lat:\t['here']\n"
+        "link:\t['https://baz.com/#/hear/?r=joe']\n"
+    )
+    actual = format_query(query)
+    assert expected == actual
+    
+
+def test_main(capsys):
+    input_lines = (
+        (
+            'https://foo.com/bar/digest'
+            '?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe\n'
+        ),
+    )
+    expected = (
+        'url:\t' 
+        "'https://foo.com/bar/digest"
+        "?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'"
+        '\n'
+
+        "scheme:\t'https'\n"
+        "netloc:\t'foo.com'\n"
+        "path:\t'/bar/digest'\n"
+        "params:\t''\n"
+        "query:\t'lat=here&link=https%3A%2F%2Fbaz.com%2F"
+            "%23%2Fhear%2F%3Fr%3Djoe'\n"
+        "fragment:\t''\n"
+
+        "lat:\t['here']\n"
+        "link:\t['https://baz.com/#/hear/?r=joe']\n"
+    )
+    main(input_lines)
+    captured = capsys.readouterr()
+    actual = captured.out
+    assert expected == actual
