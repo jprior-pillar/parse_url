dfa38ff3e290f86d2d87e297f2a12c13e1235f84
James Prior <jim.prior@accenture.com>
2020-01-10 10:55:48 -0500

    tests/test_parse_urls.py: parametrize test_main*.

--- tests/test_parse_urls.py
+++ tests/test_parse_urls.py
@@ -44,60 +44,60 @@ def test_format_query():
     assert expected == actual
 
 
-def test_main(capsys):
-    input_lines = (
-        (
-            'https://foo.com/bar/digest'
-            '?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe\n'
+test_data = (
+    (
+        ( # input_lines
+            (
+                'https://foo.com/bar/digest'
+                '?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe\n'
+            ),
         ),
-    )
-    expected = (
-        "url:\t"
-            "'https://foo.com/bar/digest"
-            "?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'"
+        ( # expected
+            "url:\t"
+                "'https://foo.com/bar/digest"
+                "?lat=here&link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'"
+                "\n"
+            "scheme:\t'https'\n"
+            "netloc:\t'foo.com'\n"
+            "path:\t'/bar/digest'\n"
+            "params:\t''\n"
+            "query:\t'lat=here&"
+                "link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'\n"
+            "fragment:\t''\n"
+            "lat:\t'here' "
+                r"(b'\x85\xea\xde')"
+                "\n"
+            "link:\t'https://baz.com/#/hear/?r=joe' "
+                r"(b'\x86\xdbi\xb3\xff\xdbk7(\x9b\xff\xe1y\xaa\xff\xae:\x1e')"
+                "\n"
+        ),
+    ),
+    (
+        ( # input_lines
+            'https://foo.com/\n',
+            'https://bar.com/\n',
+        ),
+        ( # expected
+            "url:\t'https://foo.com/'\n"
+            "scheme:\t'https'\n"
+            "netloc:\t'foo.com'\n"
+            "path:\t'/'\n"
+            "params:\t''\n"
+            "query:\t''\n"
+            "fragment:\t''\n"
             "\n"
-        "scheme:\t'https'\n"
-        "netloc:\t'foo.com'\n"
-        "path:\t'/bar/digest'\n"
-        "params:\t''\n"
-        "query:\t'lat=here&"
-            "link=https%3A%2F%2Fbaz.com%2F%23%2Fhear%2F%3Fr%3Djoe'\n"
-        "fragment:\t''\n"
-        "lat:\t'here' "
-            r"(b'\x85\xea\xde')"
-            "\n"
-        "link:\t'https://baz.com/#/hear/?r=joe' "
-            r"(b'\x86\xdbi\xb3\xff\xdbk7(\x9b\xff\xe1y\xaa\xff\xae:\x1e')"
-            "\n"
-    )
-    main(input_lines)
-    captured = capsys.readouterr()
-    actual = captured.out
-    assert expected == actual
-
-
-def test_main_two_urls(capsys):
-    input_lines = (
-        'https://foo.com/\n',
-        'https://bar.com/\n',
-    )
-    expected = (
-        "url:\t'https://foo.com/'\n"
-        "scheme:\t'https'\n"
-        "netloc:\t'foo.com'\n"
-        "path:\t'/'\n"
-        "params:\t''\n"
-        "query:\t''\n"
-        "fragment:\t''\n"
-        "\n"
-        "url:\t'https://bar.com/'\n"
-        "scheme:\t'https'\n"
-        "netloc:\t'bar.com'\n"
-        "path:\t'/'\n"
-        "params:\t''\n"
-        "query:\t''\n"
-        "fragment:\t''\n"
-    )
+            "url:\t'https://bar.com/'\n"
+            "scheme:\t'https'\n"
+            "netloc:\t'bar.com'\n"
+            "path:\t'/'\n"
+            "params:\t''\n"
+            "query:\t''\n"
+            "fragment:\t''\n"
+        ),
+    ),
+)
+@pytest.mark.parametrize('input_lines, expected', test_data)
+def test_main(input_lines, expected, capsys):
     main(input_lines)
     captured = capsys.readouterr()
     actual = captured.out
